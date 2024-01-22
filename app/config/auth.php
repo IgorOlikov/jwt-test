c59a021b70feb627152265b6653f1dcaa4139729
IgorOlikov <i.olikov@mail.ru>
2024-01-22 12:54:21 +0500

    jwt routes

--- app/config/auth.php
+++ app/config/auth.php
@@ -36,10 +36,14 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
+        'api' => [
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
+            'web' => [
+                'driver' => 'session',
+                'provider' => 'users',
+            ],
     ],
 
     /*
--- app/routes/api.php
+++ app/routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BasicAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+   Route::prefix('auth')->middleware('web')->group(function (){
+      Route::post('register',[BasicAuthController::class,'store']);
+   });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('auth')->middleware('api')->group(function () {
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::post('me', [AuthController::class,'me']);
+
 });
